eea0c60536b9ab7f05118a8aa02063f9ca70badc
garamizo <garamizo@mtu.edu>
2018-05-03 16:46:54 -0400

    redo game physics lib

--- pursuit3d/src/test.cpp
+++ pursuit3d/src/test.cpp
@@ -1,4 +1,4 @@
-#include <vectors.h>
+#include "Geometry3D.h"
 #include <iostream>
 #include <cmath>
 #include <cfloat>
@@ -6,14 +6,27 @@
 
 int main() {
 	
-	vec3 v1 = {1.0f, 3.0f, 2.0f};
-	vec3 v2 = {10.0f, 2.0f, 3.0f};
-	vec3 right = v1 - v2;
+	Point v1(5.0f, 3.0f, 2.0f);
+	Point v2 = {10.0f, 2.0f, 3.0f};
+	Line l1(v1, {10.0f, 2.0f, 3.0f});
+	Point right = v1 - v2;
 	float d = Distance(v1, v2);
+	Sphere sphere({-1, -2, -3}, 0.1f);
+	AABB box = FromMinMax(v1, {2, 5, 3});
+	// AABB box(v1, {2, 4, 3});
+	// AABB box;
+	OBB obox(v1, {1, 2, 3});
+	Ray ray(v1, {-1.0f, 0.0f, 0.0f});
+	Plane plane(v1, 0.5);
+	float d1 = PlaneEquation(v1, plane);
+	Triangle tri = FromPoints(v1, v2, {10, 2, 1});
+	Point p1 = ClosestPoint(sphere, v1);
+	Point p2 = ClosestPoint(box, v1);
 	
-	std::cout<< "Component 0: " <<right.x<< "\n";
-	std::cout<< "Component 0: " <<right.asArray[0] << "\n";
-	std::cout<< "Component 0: " <<right[0] << "\n";
+	std::cout<< "Component 0: " << AABBOBB(box, obox) << "\n";
+	std::cout<< "Component 0: " << AABBPlane(box, plane) << "\n";
+	std::cout<< "Component 0: " << Linetest(obox, l1) << "\n";
+	std::cout<< "Component 0: " << Raycast(plane, ray) << "\n";
 	
 	return 0;
 }
