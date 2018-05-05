efa4210051b1112df4636535a47b7a4b503c6b1f
garamizo <garamizo@mtu.edu>
2018-05-05 00:26:42 -0400

    fix ground truth error

--- pursuit3d/scripts/publish_robots.py
+++ pursuit3d/scripts/publish_robots.py
@@ -7,10 +7,12 @@ import tf
 def odometry_cb(msg):
 
     pose = msg.pose.pose
+    quat = np.array([pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w])
+    quat = quat / np.sqrt(np.sum(quat**2))
     br.sendTransform((pose.position.x, pose.position.y, pose.position.z),
-                     (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w),
-                     msg.header.stamp,
-                     "base_link",
+                     (quat[0], quat[1], quat[2], quat[3]),
+                     rospy.Time.now(),
+                     "base_stabilized",
                      "map")
 
 
